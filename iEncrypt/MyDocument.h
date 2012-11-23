e760f6401b48ce24d13f220391592e76c1dd2933
tian <tian@tians-iMac.local>
2012-11-22 22:33:27 -0500

    Initial Commit

---
+++ iEncrypt/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  iEncrypt
+//
+//  Created by tian on 11/22/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument {
+}
+
+@end
